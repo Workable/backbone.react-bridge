cdf9e27bc023f54b9a03053f3a4d14ed3f6151bc
vorillaz <theodorevorillas@gmail.com>
2017-01-31 01:53:22 +0200

    Update tests to use AVA

---
+++ __tests__/componentFromView.test.js
@@ -0,0 +1,134 @@
+import _ from 'underscore';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-addons-test-utils';
+import initStore from 'redux-test-belt';
+import ReactBridge from '../src';
+
+import { describe } from 'ava-spec';
+import sinon from 'sinon'
+
+import Collection from '../examples/templates/marionetteCollection';
+import {viewOptions} from '../examples/helpers';
+
+describe('componentFromView', it => {
+
+  it.beforeEach(() => {
+    document.body.innerHTML = '<div id="app"></div>';
+  });
+
+  it('creates a React.Component from a Marionette.View Class', t => {
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    t.true(ReactTestUtils.isElement(<ReactComponent />));
+  });
+
+  it('creates a React.Component from a Marionette.View', t => {
+    const ReactComponent = ReactBridge.componentFromView(new Collection(), viewOptions);
+    t.true(ReactTestUtils.isElement(<ReactComponent />));
+  });
+
+  it('renders a React.Component from a Marionette.View', t => {
+    let element = null;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (element = el)}/>, document.getElementById('app'));
+
+    const listItems = ReactDOM.findDOMNode(element).children;
+
+    t.is(listItems.length, 2);
+    t.is(listItems[0].textContent, 'Bacon - Should buy ASAP!');
+    t.is(listItems[1].textContent, 'Vegetables - Forget it..');
+  });
+
+  it('wraps the the React.Component with an element with default tag according to Marionette', t => {
+    let element = null;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (element = el)}/>, document.getElementById('app'));
+
+    const tagName = ReactDOM.findDOMNode(element).tagName;
+    t.is(tagName, 'UL');
+  });
+
+  it('wraps the the React.Component with an element without className', t => {
+    let element = null;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (element = el)}/>, document.getElementById('app'));
+
+    const tagName = ReactDOM.findDOMNode(element).className;
+    t.is(tagName, '');
+  });
+
+  it('wraps the the React.Component with an element with custom tag', t => {
+    let element = null;
+    const customViewOptions = _.extend({}, viewOptions);
+    customViewOptions.tagName = 'div';
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, customViewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (element = el)}/>, document.getElementById('app'));
+
+    const tagName = ReactDOM.findDOMNode(element).tagName;
+    t.is(tagName, 'DIV');
+  });
+
+  it('wraps the the React.Component with an element with custom className', t => {
+    let element = null;
+    const customViewOptions = _.extend({}, viewOptions);
+    customViewOptions.className = 'myClass';
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, customViewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (element = el)}/>, document.getElementById('app'));
+
+    const tagName = ReactDOM.findDOMNode(element).className;
+    t.is(tagName, 'myClass');
+  });
+
+  it('calls a function of the Marionette View', t => {
+    let viewInstance = null;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    ReactDOM.render(<ReactComponent ref={el => (viewInstance = el)} />, document.getElementById('app'));
+
+    const spy = sinon.spy(viewInstance, 'delegate');
+    viewInstance.delegate('log', 'this is a test');
+
+    t.true(spy.calledOnce);
+    t.true(spy.calledWith('log', 'this is a test'));
+  });
+
+  it('throws an error when eventsToActions function exists without defining a dispatch function', t => {
+    const spy = sinon.spy(console, 'error');
+    const customViewOptions = _.extend({}, viewOptions);
+    delete customViewOptions.dispatch;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, customViewOptions);
+    ReactDOM.render(<ReactComponent />, document.getElementById('app'));
+
+    t.true(spy.calledOnce);
+    t.true(spy.calledWith('The `dispatch` function is not defined.'));
+  });
+
+  it('dispatches an action when the view triggers an event', t => {
+    const mockStore = initStore();
+    const store = mockStore({});
+    const customViewOptions = _.extend({}, viewOptions);
+    customViewOptions.dispatch = store.dispatch;
+
+    const ReactComponent = ReactBridge.componentFromView(Collection, customViewOptions);
+    ReactDOM.render(<ReactComponent />, document.getElementById('app'));
+
+    t.true(store.hasActions({type: 'INIT'}));
+  });
+
+
+  it('destroys the Marionette.View rendered inside the React.Component', t => {
+    const ReactComponent = ReactBridge.componentFromView(Collection, viewOptions);
+    const reactInstance = ReactDOM.render(<ReactComponent/>, document.getElementById('app'));
+
+    reactInstance.componentWillUnmount();
+
+    const marionetteViewDestroyed = reactInstance._marionetteView.isDestroyed;
+    t.true(marionetteViewDestroyed)
+  });
+});
---
+++ __tests__/viewFromComponent.test.js
@@ -0,0 +1,157 @@
+import _ from 'underscore';
+import Backbone from 'backbone';
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import ReactBridge from '../src';
+
+import { describe } from 'ava-spec';
+
+import Layout from '../examples/templates/marionetteLayout';
+import Component from '../examples/templates/reactComponent';
+import {reactConfig, MyCollection} from '../examples/helpers';
+
+describe('viewFromComponent', it => {
+
+  it.beforeEach(() => {
+    document.body.innerHTML = '<div id="app"></div>';
+  });
+
+  it('renders the Marionette.View created from the React.Component Class', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+    LayoutView.render().getRegion('component').show(MarionetteView);
+
+    t.false(_.isUndefined(LayoutView.component.currentView));
+  });
+
+  it('renders the Marionette.View created from the React.Component', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(<Component />, reactConfig);
+    LayoutView.render().getRegion('component').show(MarionetteView);
+
+    t.false(_.isUndefined(LayoutView.component.currentView));
+  });
+
+  it('Marionette.View has a Backbone Model', t => {
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+    t.true(MarionetteView.model instanceof Backbone.Model)
+  });
+
+  it('Marionette.View has a Backbone Collection', t => {
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+    t.true(MarionetteView.collection instanceof Backbone.Collection)
+  });
+
+  it('the created React.Component has a default getProps function', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+    LayoutView.render().getRegion('component').show(MarionetteView);
+
+    const model = reactConfig.model.toJSON();
+    const collection = reactConfig.collection.toJSON();
+    const reactState = MarionetteView._reactInternalInstance.state;
+
+    _.each(_.keys(model), (key) => {
+      t.is(reactState[key], model[key]);
+    });
+
+    t.deepEqual(reactState.items, collection);
+  });
+
+  it('the default getProps function of the React.Component is overridable', t => {
+    const customReactConfig = _.extend({}, reactConfig);
+    const collection = MyCollection.toJSON();
+
+    customReactConfig.getProps = () => (_.extend({}, {items: collection}));
+
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, customReactConfig);
+    LayoutView.render().getRegion('component').show(MarionetteView);
+
+    const reactState = MarionetteView._reactInternalInstance.state;
+    t.deepEqual(reactState.items, collection);
+  });
+
+
+  it('the React.Component may has props which override the default Marionette.View Model', t => {
+    const customReactConfig = _.extend({}, reactConfig);
+    customReactConfig.props = {
+      title : 'I am a React Component inside a Marionette View'
+    };
+
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, customReactConfig);
+    LayoutView.render().getRegion('component').show(MarionetteView);
+
+    const reactState = MarionetteView._reactInternalInstance.state;
+    t.is(reactState.title, 'I am a React Component inside a Marionette View');
+  });
+
+  it('renders properly the React.Component', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+
+    const renderedView = LayoutView.render().getRegion('component').show(MarionetteView);
+    const reactInstance = renderedView.currentView._reactInternalInstance;
+
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(reactInstance, 'button');
+    const textAreas = ReactTestUtils.scryRenderedDOMComponentsWithTag(reactInstance, 'textarea');
+
+    require('assert').ok(LayoutView);
+    t.is(buttons.length, 1);
+    t.is(buttons[0].textContent, 'Click Me');
+    t.is(textAreas.length, 1);
+    t.is(textAreas[0].placeholder, 'Please insert your text..');
+  });
+
+  it('React.Component responds to Backbone.Model changes', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+
+    const renderedView = LayoutView.render().getRegion('component').show(MarionetteView);
+    const reactInstance = renderedView.currentView._reactInternalInstance;
+
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(reactInstance, 'button');
+
+    reactConfig.model.set({buttonText: 'Oh hooray!'});
+
+    t.is(buttons.length, 1);
+    t.is(buttons[0].textContent, 'Oh hooray!');
+  });
+
+  it('React.Component responds to Backbone.Model changes triggered by custom events', t => {
+    //define a custom model event listener
+    reactConfig.observe = {
+      model: 'customEvent'
+    };
+
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+
+    const renderedView = LayoutView.render().getRegion('component').show(MarionetteView);
+    const reactInstance = renderedView.currentView._reactInternalInstance;
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(reactInstance, 'button');
+
+    reactConfig.model.set({buttonText: 'Oh hooray!'});
+    reactConfig.model.trigger('customEvent');
+
+    t.is(buttons.length, 1);
+    t.is(buttons[0].textContent, 'Oh hooray!');
+  });
+
+  it('destroys the Marionette.View rendered inside the React.Component', t => {
+    const LayoutView = new Layout({el: '#app'});
+    const MarionetteView = ReactBridge.viewFromComponent(Component, reactConfig);
+
+    const renderedView = LayoutView.render().getRegion('component').show(MarionetteView);
+    const reactInstance = renderedView.currentView._reactInternalInstance;
+
+    MarionetteView.destroy();
+
+    const reactInstanceDestroyed = reactInstance._isMounted;
+    t.false(reactInstanceDestroyed)
+
+    const marionetteViewDestroyed = MarionetteView.isDestroyed;
+    t.true(marionetteViewDestroyed)
+  });
+});
